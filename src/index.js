e53fcd9b2e1653432cd5a7e72456044cf2e0c507
thenam153 <noilinhtinh016@gmail.com>
2019-09-11 17:25:24 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var moduleName = 'i2gDataApp';
-var app = angular.module(moduleName, ['file-explorer', 'wiApi', 'wiTreeViewVirtual','angularModalService']);
+var app = angular.module(moduleName, ['file-explorer', 'wiApi', 'wiTreeViewVirtual','angularModalService','wiDroppable']);
 
 app.controller('mainCtrl', function($scope, wiApi, $timeout, $http) {
 	console.log('main ctrl');
@@ -86,6 +86,21 @@ app.controller('mainCtrl', function($scope, wiApi, $timeout, $http) {
         	$timeout(() => {
         		self.listUser = data;
         	})
+        })
+    }
+    self.storageDatabaseWiDrop = {
+        company: "I2G",
+        directory: "3dbd0737b06bce192340608ff19b68109c23e22a",
+        name: "I2G-thangnguyen",
+    }
+    this.onDrop = function(event, helper, data) {
+        console.log(data);
+        $timeout(() => {
+            self.storageDatabaseWiDrop = {
+                                         company: "I2G",
+                                         directory: "65554a26e839dfb6fc5bc9f392f15d90764cabe4",
+                                         name: "I2G-namnt",
+                                        }
         })
     }
 });
