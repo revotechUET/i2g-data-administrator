68cf76727ed7886663dd505c036f5a56aeef2540
Hung <hungnk1996@gmail.com>
2019-10-04 15:28:10 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,16 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'main.bundle.js',
-		path: __dirname + '/dist'
+		path: __dirname + '/public/dist'
 	},
-	mode: 'development'
+	mode: 'development',
+	module: {
+		rules: [{
+			test: /\.html$/,
+			use: ['html-loader']
+		}, {
+			test: /\.less$/,
+			use: ["style-loader", "css-loader", "less-loader"]
+		}]
+	}
 }
