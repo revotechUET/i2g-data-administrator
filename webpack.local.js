e79042eb27e5847aa5ea9fa26ffda7c7ed7bac66
phucnv282 <phucnv282@gmail.com>
2020-03-19 15:36:18 +0700

    update

---
+++ webpack.local.js
@@ -0,0 +1,21 @@
+var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.bundle.js',
+		path: __dirname + '/public/dist'
+	},
+	mode: 'development',
+	module: {
+		rules: [{
+			test: /\.html$/,
+			use: ['html-loader']
+		}, {
+			test: /\.less$/,
+			use: ["style-loader", "css-loader", "less-loader"]
+		}]
+	},
+  plugins: [
+    new HardSourceWebpackPlugin()
+  ]
+}
